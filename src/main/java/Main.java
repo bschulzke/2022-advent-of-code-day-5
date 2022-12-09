aa179fab5be44f3444d8adc0f6f89890e9b6024b
Ben Schulzke <benschulzke@gmail.com>
2022-12-09 00:05:29 -0700

    complete part one

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,12 @@
+import javax.print.DocFlavor;
 import java.io.File;
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class Main {
 
   private static Map<Integer, Stack<Character>> stacks = new HashMap<>();
+  private static ArrayList<String> moves = new ArrayList<>();
 
   public static void main(String[] args) {
 
@@ -19,8 +22,12 @@ public class Main {
       }
 
       for (String line : lines) {
-        if (line.contains("1")) {
-          break;
+        if (line.contains(" 1   2   3   4   5   6   7   8   9")) {
+          continue;
+        }
+        if (line.contains("move")) {
+          moves.add(getMove(line));
+          continue;
         }
         for (int i = 0; i < line.length(); i += 4) {
           int stackIndex = i / 4 + 1;
@@ -33,9 +40,15 @@ public class Main {
 
       reverseAllStacks();
 
-      for (int i = 1; i <= 9; i++) {
-        System.out.println(i + ": " + stacks.get(i));
+      for (String move : moves) {
+        completeMove(move);
       }
+
+      StringBuilder builder = new StringBuilder();
+      for (Stack<Character> stack : stacks.values()) {
+        builder.append(stack.peek());
+      }
+      System.out.println(builder.toString());
 
     } catch (Exception e) {
       System.out.println("An error occurred.");
@@ -66,9 +79,35 @@ public class Main {
     }
   }
 
-  private static void moveFromAtoB(Stack<Character> a, Stack<Character> b) {
+  private static void completeMove(String move) {
+    int numToMove;
+    int from;
+    int to;
+    if (move.length() < 4) {
+      numToMove = Integer.parseInt(move.substring(0, 1));
+      from = Integer.parseInt(move.substring(1, 2));
+      to = Integer.parseInt(move.substring(2, 3));
+    } else {
+      numToMove = Integer.parseInt(move.substring(0, 2));
+      from = Integer.parseInt(move.substring(2, 3));
+      to = Integer.parseInt(move.substring(3, 4));
+    }
+    for (int i = 1; i <= numToMove; i++) {
+      moveFromStackAtoB(stacks.get(from), stacks.get(to));
+    }
+  }
+
+  private static void moveFromStackAtoB(Stack<Character> a, Stack<Character> b) {
     Character target = a.pop();
     b.push(target);
+  }
+
+  private static String getMove(String line) {
+    return line
+            .replace("move", "")
+            .replace("from", "")
+            .replace("to", "")
+            .replace(" ", "").trim();
   }
 
 }
